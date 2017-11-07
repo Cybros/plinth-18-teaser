95dd0d3b8f835407e33c31be563d7096e1bbfbf0
Shubham Mangal <shubham.mangal15@gmail.com>
2017-11-07 21:09:42 +0530

    font color changes

--- public/js/script.js
+++ public/js/script.js
@@ -1,46 +1,22 @@
-$(document).ready(function () {
 
-      	$(window).scroll(function() {
-		    if ($(document).scrollTop() > 200) {
-		        $('.nav').addClass("hidden");
-		    } else {
-		        $('.nav').removeClass("hidden");
-		    }
-		});
 
-        var carousel = $("#carousel").waterwheelCarousel({
-          flankingItems: 3,
-          movingToCenter: function ($item) {
-            $('#callback-output').prepend('movingToCenter: ' + $item.attr('id') + '<br/>');
-          },
-          movedToCenter: function ($item) {
-            $('#callback-output').prepend('movedToCenter: ' + $item.attr('id') + '<br/>');
-          },
-          movingFromCenter: function ($item) {
-            $('#callback-output').prepend('movingFromCenter: ' + $item.attr('id') + '<br/>');
-          },
-          movedFromCenter: function ($item) {
-            $('#callback-output').prepend('movedFromCenter: ' + $item.attr('id') + '<br/>');
-          },
-          clickedCenter: function ($item) {
-            $('#callback-output').prepend('clickedCenter: ' + $item.attr('id') + '<br/>');
-          }
-        });
+			$(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+    var homeHeight = $('#home').height() -22;
+    console.log(homeHeight,scroll);
+    if(scroll > homeHeight){
+        $('.nav-item a').css('color','black');
+        $('.nav-link').css('color','black');
+        $('.nav').addClass("hidden");
+    }
+    else {
+			$('.nav-item a').css('color','white');
+			$('.nav-link').css('color','white');
+      $('.nav').removeClass("hidden");
+    }
 
-        $('#prev').bind('click', function () {
-          carousel.prev();
-          return false
-        });
+    // for logo
 
-        $('#next').bind('click', function () {
-          carousel.next();
-          return false;
-        });
-
-        $('#reload').bind('click', function () {
-          newOptions = eval("(" + $('#newoptions').val() + ")");
-          carousel.reload(newOptions);
-          return false;
-        });
-
-      });
+    
+    
+});
